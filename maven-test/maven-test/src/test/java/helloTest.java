4484c90f59e52c7172d42cfdaa17d008e54a1392
henrycoffee <henrycoffee@163.com>
2024-08-01 16:01:39 +0800

    maven使用junit测试，生成测试报告和测试覆盖率报告

---
+++ maven-test/maven-test/src/test/java/helloTest.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class helloTest {
+
+    @Test
+    public void test1() {
+        Assert.assertEquals(0,1);
+    }
+
+    @Test
+    public void test2() {
+        Assert.assertEquals(1,1);
+    }
+}
